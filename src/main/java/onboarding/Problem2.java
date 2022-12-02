ace8e86e913fd1fc7beb89c28405a24320a84b34
jinwon <kjw55523@gmail.com>
2022-12-02 12:36:45 +0900

    problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,30 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+
+        return removeDulicate(cryptogram);
+    }
+
+    public static String removeDulicate(String cryptogram){
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<cryptogram.length(); i++){
+            //핵심
+            if(!stack.isEmpty() && stack.peek() == cryptogram.charAt(i))
+                stack.pop();
+            else
+                stack.push(cryptogram.charAt(i));
+        }
+        return printCrypto(stack);
+    }
+
+    private static String printCrypto(Stack<Character> stack) {
+        String answer = "";
+        for (int i = 0; i < stack.size(); i++) {
+            answer += stack.get(i) + "";
+        }
         return answer;
     }
 }
